c8c0bb8e50fb5e7141a474f22120e0cfe32cfd98
mike <mikebell90@yahoo.com>
2014-09-04 09:52:52 -0700

    more excpetion handling

--- src/main/java/com/force/api/SObjectException.java
+++ src/main/java/com/force/api/SObjectException.java
@@ -1,10 +1,20 @@
 package com.force.api;
 
-public class SObjectException extends RuntimeException {
+public class SObjectException extends SFApiException {
 
 	private static final long serialVersionUID = 1L;
 
+	private ApiError[] errors;
+	
 	public SObjectException(ApiError[] errors) {
-		
+		this.errors=errors;
+	}
+
+	public ApiError[] getErrors() {
+		return this.errors;
+	}
+
+	public void setErrors(ApiError[] errors) {
+		this.errors = errors;
 	}
 }
