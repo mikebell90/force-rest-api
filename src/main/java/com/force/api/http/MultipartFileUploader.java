ebefb871b852f98b0bc05a30761d2bab3f677c67
mike <mikebell90@yahoo.com>
2014-10-10 09:48:59 -0700

    addToFile allows a static upload of a file to chatter. This one is unconnected, but...

--- src/main/java/com/force/api/http/MultipartFileUploader.java
+++ src/main/java/com/force/api/http/MultipartFileUploader.java
@@ -3,6 +3,7 @@ package com.force.api.http;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -49,6 +50,8 @@ public class MultipartFileUploader {
 			System.out.println(u.upload(sources, requestURL, session).getString());
 		}
 	}
+	
+	// DOES NOT WORK!
 	public static void addToChatter(String parentID,String session) throws Exception {
 		MultipartFileUploader u=new MultipartFileUploader();
 		parentID="003o0000003POisAAG";
@@ -72,6 +75,25 @@ public class MultipartFileUploader {
 		}
 	}
 	
+	public static void addToFile(String title,String filename,String userID,String session) throws Exception {
+		MultipartFileUploader u=new MultipartFileUploader();
+		
+		session="00Do0000000aTGT!AQwAQE0SUSwsmJTewEsy12HnX1LtdymLwe5U9DfwBH90rl7o5qmT43gN4tJpdgyj9zcKcalrBSmcnwEQ_yj7P76uxplLfKjD";
+		List<DataSourceWithFileName> sources=new ArrayList<DataSourceWithFileName>();
+		DataSourceWithFileName json=
+				new StringDataSource("{ \"title\" : \""+title+"\" }");
+		sources.add(json);
+		// needs better closing
+		try (InputStream is=new FileInputStream("c:\\users\\michael\\desktop\\test.ods"))  {
+			DataSourceWithFileName data=new InputStreamDataSource(is, "fileData",filename);
+			sources.add(data);
+			String requestURL="https://na17.salesforce.com/services/data/v32.0/chatter/users/{0}/files";
+			
+			System.out.println(u.upload(sources, MessageFormat.format(requestURL, userID), session).getString());
+		}
+	}
+	
+	
 	public static void addToFolder(String folderID,String session) throws Exception {
 		MultipartFileUploader u=new MultipartFileUploader();
 		folderID="00lo0000000bo4MAAQ";
@@ -90,6 +112,6 @@ public class MultipartFileUploader {
 	}
 	
 	public static void main(String[] args) throws Exception {
-	 addToChatter(null,null);
+	 addToFile("This is a title of a file", "me", null);
 }	
 }	
